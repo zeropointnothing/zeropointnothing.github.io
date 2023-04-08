c395998464eabbae7655a396a76169b9cb03d3bd
ZeroPointNothing <116678675+ZeroPointNothing@users.noreply.github.com>
2023-04-08 01:03:28 -0400

    added fix for mobile squeak

--- discord_stuff/index.js
+++ discord_stuff/index.js
@@ -1,6 +1,6 @@
 // Toggle the hamburger menu so it can be open.
-$(document).ready(function() {
-  $(".menu-container").find(".hamburger").click(function() {
+$(document).ready(function () {
+  $(".menu-container").find(".hamburger").click(function () {
     $(".menu-container").toggleClass("open");
   });
 });
@@ -11,55 +11,77 @@ var interval;
 var lsize = 200
 var ssize = 150
 
+var frogImg = document.getElementById("frogImg");
 
-document.getElementById("frogImg").onmousedown = function () {
-  if (ssqueak.paused) {
-    ssqueak.play();
-  } else {
-    ssqueak.currentTime = 0;
-  }
+var factor = 0.9;
+var interval;
+var lsize = 200
+var ssize = 150
 
-  clearInterval(interval);
-  var img = document.getElementById("frogImg");
-  var canvas = document.createElement("canvas");
-  canvas.width = img.width;
-  canvas.height = img.height;
-  var ctx = canvas.getContext("2d");
-  ctx.drawImage(img, 0, 0);
-  interval = setInterval(function () {
-    if (img.width * factor < ssize || img.height * factor < ssize) {
-      clearInterval(interval);
+
+function handleStart(event) {
+  if (event.type === "mousedown" || event.targetTouches.length == 1) {
+    if (ssqueak.paused) {
+      ssqueak.play();
     } else {
-      img.width *= factor;
-      img.height *= factor;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(img, 0, 0);
+      ssqueak.currentTime = 0;
     }
-  }, 10);
+
+    clearInterval(interval);
+    var img = document.getElementById("frogImg");
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    interval = setInterval(function () {
+      if (img.width * factor < ssize || img.height * factor < ssize) {
+        clearInterval(interval);
+      } else {
+        img.width *= factor;
+        img.height *= factor;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0);
+      }
+    }, 10);
+    if (event.type === "touchstart") {
+      event.preventDefault();
+    }
+  }
 }
 
-document.getElementById("frogImg").onmouseup = function () {
-  if (esqueak.paused) {
-    esqueak.play();
-  } else {
-    esqueak.currentTime = 0;
-  }
-
-  clearInterval(interval);
-  var img = document.getElementById("frogImg");
-  var canvas = document.createElement("canvas");
-  canvas.width = img.width;
-  canvas.height = img.height;
-  var ctx = canvas.getContext("2d");
-  ctx.drawImage(img, 0, 0);
-  interval = setInterval(function () {
-    if (img.width / factor > lsize || img.height / factor > lsize) {
-      clearInterval(interval);
+function handleEnd(event) {
+  if (event.type === "mouseup" || event.changedTouches.length == 1) {
+    if (esqueak.paused) {
+      esqueak.play();
     } else {
-      img.width /= factor;
-      img.height /= factor;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(img, 0, 0);
+      esqueak.currentTime = 0;
     }
-  }, 10);
+
+    clearInterval(interval);
+    var img = document.getElementById("frogImg");
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    interval = setInterval(function () {
+      if (img.width / factor > lsize || img.height / factor > lsize) {
+        clearInterval(interval);
+      } else {
+        img.width /= factor;
+        img.height /= factor;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0);
+      }
+    }, 10);
+    if (event.type === "touchend") {
+      event.preventDefault();
+    }
+  }
 }
+
+document.getElementById("frogImg").addEventListener("touchstart", handleStart, false);
+document.getElementById("frogImg").addEventListener("touchend", handleEnd, false);
+document.getElementById("frogImg").addEventListener("mousedown", handleStart, false);
+document.getElementById("frogImg").addEventListener("mouseup", handleEnd, false);
